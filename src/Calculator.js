8750827dadbcb54fe94243bb6cbf08c06353e9fd
Preston Doris <prestondoris@gmail.com>
2020-08-12 09:10:52 -0700

    Updated Calculator Grid to include period button

--- src/Calculator.js
+++ src/Calculator.js
@@ -27,6 +27,7 @@ class Calculator extends Component {
         <div className='area'>3</div>
         <div className='area'>-</div>
         <div className='b17 area'>0</div>
+        <div className='area'>.</div>
         <div className='area'>=</div>
       </main>
     )
