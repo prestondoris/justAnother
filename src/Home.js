b34ebb644149b6a82f8e1eb40ed9406f513a9c37
Preston Doris <prestondoris@gmail.com>
2020-08-25 15:22:25 -0700

    Updated name in Navbar

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ function Home() {
   let cards = ['Calculator', 'Stopwatch']
   return (
     <div>
-      <h2>Welcome to Just Another...</h2>
-      <p>This is a site to provide just another simple to use common... </p>
+      <h2>Welcome to UtilWeb.io</h2>
+      <p>This is a site to provide simple Utilities for the Web</p>
       <Cards cards={cards} />
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-contents">
         <p className='home'>
-          <Link id="homeLink" to="/">Just Another...</Link>
+          <Link id="homeLink" to="/">UtilWeb.io</Link>
         </p>
         <ul className='list'>
           <li className='list-item'><NavLink activeClassName='active' className="pageLinks" to="/Calculator">Calculator</NavLink></li>
